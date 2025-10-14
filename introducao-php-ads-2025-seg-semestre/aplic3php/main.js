9f959879a80d796cab4e7dd6cbe3172d609e7a25
niveabeh <niveasouza86@gmail.com>
2025-10-13 22:41:07 -0300

    add aplic3php

---
+++ introducao-php-ads-2025-seg-semestre/aplic3php/main.js
@@ -0,0 +1,16 @@
+const frm = document.getElementById("formulario");
+const inicio = document.getElementById("inicio");
+const fim = document.getElementById("fim");
+const resp = document.getElementById("resp");
+
+frm.addEventListener("submit", (e) =>{ 
+    e.preventDefault(); 
+    let valorInicio = inicio.value.trim();
+    let valorFim = fim.value.trim();
+    resp.
+    
+    for(let i = valorInicio; i <= valorFim; i++){
+        
+    }
+     
+})
